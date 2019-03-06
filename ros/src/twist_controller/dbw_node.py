9661089e7840d3ec857a413748c39753dfd93219
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-03-06 09:01:25 -0800

    - Initial implementation for dbw_node and twist_controller

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -34,6 +34,13 @@ that we have created in the `__init__` function.
 class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
+	
+	self.current_vel = None
+	self.curr_ang_vel = None
+	self.dbw_enabled = None
+	self.linear_vel = None
+	self.angular_vel = None
+	self.throttle = self.steering = self.brake = 0
 
         vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
         fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
@@ -72,7 +79,23 @@ class DBWNode(object):
             #                                                     <any other argument you need>)
             # if <dbw is enabled>:
             #   self.publish(throttle, brake, steer)
+	    
+	    if not None in (self.current_vel, self.linear_vel, self.angular_vel):
+		self.throttle, self.brake, self.steering = self.controller.control(self.current_vel, self.dbw_enabled, self.linear_vel, self.angular_vel)
+		if(self.dbw_enabled):
+		    self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
+
+    def dbw_enabled_cb(self, msg):
+	self.dbw_enabled = msg
+
+    def twist_cb(self, msg):
+	self.linear_vel = msg.twist.linear.x
+	self.angular_vel = msg.twist.angular.z
+
+    def velocity_cb(self, msg):
+	self.current_vel = msg.twist.linear.x
+	
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
